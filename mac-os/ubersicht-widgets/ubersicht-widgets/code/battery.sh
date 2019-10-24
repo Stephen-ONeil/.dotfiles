6b6d509503d0bf14e04f26e18f9bb82e6e9967cf
Stephen-ONeil <stephenlevioneil@gmail.com>
2019-10-24 14:27:32 -0400

    Customize battery widget

--- mac-os/ubersicht-widgets/ubersicht-widgets/code/battery.sh
+++ mac-os/ubersicht-widgets/ubersicht-widgets/code/battery.sh
@@ -2,25 +2,21 @@
 
 BATTERY=$(pmset -g batt)
 
-PERCENT=$(echo "$BATTERY" | grep -oE '\d*%' | tr -d '%')
-
 SOURCE=$(echo "$BATTERY" | grep -oE "'.*?'" | sed -E "s/'(.*) Power'/\1/")
-
 if [[ "$SOURCE" == 'AC' ]]; then
-    ICON='plug'
+    ICON="+"
+    TIME_REMAINING=""
 else
-    if [[ "$PERCENT" -lt 10 ]]; then
-        LVL='empty'
-    elif [[ "$PERCENT" -lt 35 ]]; then
-        LVL='quarter'
-    elif [[ "$PERCENT" -lt 65 ]]; then
-        LVL='half'
-    elif [[ "$PERCENT" -lt 90 ]]; then
-        LVL='three-quarters'
-    else
-        LVL='full'
-    fi
-    ICON="battery-$LVL"
+    ICON="-"
+    TIME_REMAINING=$(echo "$BATTERY" | grep -oE '[^ ]* remaining')
 fi
 
-echo "@${ICON}@ ${PERCENT}%"
+PERCENT=$(echo "$BATTERY" | grep -oE '\d*%' | tr -d '%')
+if [[ "$PERCENT" -lt 20 ]]; then
+    status_style="color: red"
+else
+    status_style=""
+fi
+PERCENT_DISPLAY="<span style='$status_style'>$PERCENT</span>%"
+
+echo "${ICON} ${PERCENT_DISPLAY} ${TIME_REMAINING}"
